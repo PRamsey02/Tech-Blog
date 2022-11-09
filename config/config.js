874804329a02eaa9f1a1ebcb9cf27b2b9955a9e3
PRamsey02 <prestonkramsey02@icloud.com>
2022-11-08 23:21:26 -0600

    config.js trial

--- config/config.js
+++ config/config.js
@@ -3,15 +3,29 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 // Create connection to our db
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+// const sequelize = process.env.JAWSDB_URL
+//   ? new Sequelize(process.env.JAWSDB_URL)
+//   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+//       host: 'localhost',
+//       user: 'root',
+//       password: 'password',
+//       database: 'tech_blog_db',
+//       dialect: 'mysql',
+//     });
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
       host: 'localhost',
-      user: 'root',
-      password: 'password',
-      database: 'tech_blog_db',
       dialect: 'mysql',
-      // port: 3306
-    });
+      port: 3306,
+    }
+  );
+}
 
 module.exports = sequelize;
